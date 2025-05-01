9d56a3c10eae9d41b971d877be440684c4d1edfe
PhilDefi <philippe_deflandre@yahoo.fr>
2025-05-01 09:42:28 +0200

    Add a basic POST endpoint for test

--- API_credit.py
+++ API_credit.py
@@ -3,6 +3,8 @@
 
 from fastapi import FastAPI, HTTPException
 import mlflow
+from pydantic import BaseModel
+from typing import List
 
 # On Anaconda command prompt :
 # cd documents/python/projets/projet_7
@@ -14,4 +16,12 @@ app = FastAPI(title="Loan Default Prediction API")
 @app.get("/")
 def root():
     return {"message": "API is running!!!"}   
+
+class PredictRequest(BaseModel):
+    data: List[List[float]]
+    columns: List[str]
+
+@app.post("/predict")
+def predict(request: PredictRequest):
+    return {"message": "Received your request!", "data_shape": [len(request.data), len(request.columns)]}
 
